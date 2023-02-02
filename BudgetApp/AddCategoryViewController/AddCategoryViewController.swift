629183852b6762310291c7856a6e1455372b40ba
Nikita <55090468+banger980@users.noreply.github.com>
2023-02-02 20:48:48 +0300

    makingAddController

---
+++ BudgetApp/AddCategoryViewController/AddCategoryViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AddCategoryViewController.swift
+//  BudgetApp
+//
+//  Created by Nikita on 2.02.23.
+//
+
+import UIKit
+
+class AddCategoryViewController: UIViewController {
+    
+  
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Выберите группу"
+    }
+   
+    
+    @IBAction func deleteSheetController(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func makeYourCategory(_ sender: UIButton) {
+    }
+    
+}
---
+++ BudgetApp/BillViewController/BillViewController.swift
@@ -0,0 +1,64 @@
+//
+//  BillViewController.swift
+//  BudgetApp
+//
+//  Created by Nikita on 2.02.23.
+//
+
+import UIKit
+
+class BillViewController: UIViewController {
+
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    
+    func showMyViewControllerInACustomizedSheet() {
+        let viewControllerToPresent = AddCategoryViewController(nibName: "AddCategoryViewController", bundle: nil)
+        if let sheet = viewControllerToPresent.sheetPresentationController {
+            sheet.detents = [.medium(), .large()]
+            sheet.largestUndimmedDetentIdentifier = .medium
+            sheet.prefersScrollingExpandsWhenScrolledToEdge = false
+            sheet.prefersEdgeAttachedInCompactHeight = true
+            sheet.widthFollowsPreferredContentSizeWhenEdgeAttached = true
+        }
+        present(viewControllerToPresent, animated: true, completion: nil)
+    }
+    
+    
+
+    @IBAction func sumNumberToCounterDidTap(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.3) {
+            if self.counterLabel.text != "0" {
+                self.counterLabel.text = String("\(self.counterLabel.text!)\(sender.tag - 1000)")
+            } else {
+                self.counterLabel.text = String(sender.tag - 1000)
+            }
+        }
+    }
+    
+    @IBAction func moveToSettingsButton(button sender: UIButton) {
+        if self.counterLabel.text != "0" {
+            showMyViewControllerInACustomizedSheet()
+        }
+    }
+    
+    @IBAction func deleteLastNumberFromCounter(_ sender: UIButton) {
+        if self.counterLabel.text == "0" {
+            
+        } else {
+            if self.counterLabel.text!.count > 1 {
+                self.counterLabel.text!.removeLast()
+            } else {
+                self.counterLabel.text = "0"
+            }
+        }
+    }
+    
+    
+}
